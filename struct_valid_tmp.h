4dd927f306a7fe41419c4473d8c00ca822a35610
tomekgro <grodek.tomasz@gmail.com>
2016-06-03 19:52:30 +0200

    Update struct_valid_tmp.h

--- struct_valid_tmp.h
+++ struct_valid_tmp.h
@@ -19,7 +19,6 @@ typedef struct Memory Memory;
 typedef struct TreeWalk TreeWalk;
 
 struct TreeWalk{
-  int left, right, up;
   TreeWalk *p_left, *p_right, *p_parent;
   int info, key, depth, weight;
 };
@@ -51,14 +50,9 @@ void struct_change_nothing2(Memory*);
   @    predicate valid_TreeWalk_ptr{L}(TreeWalk *k);
   @
   @    predicate valid_TreeWalk{L}(TreeWalk k)=
-  @       (k.left==go || k.left==stop) && (k.right==go || k.right==stop)&&
-  @       (k.up==go || k.up==stop)&&
-  @       (k.up==go ==> valid_TreeWalk_ptr{L}(k.p_parent))&&
-  @       (k.left==go ==> valid_TreeWalk_ptr{L}(k.p_left)) &&
-  @       (k.right==go ==> valid_TreeWalk_ptr{L}(k.p_right))&&
-  @       (k.left==stop ==> k.p_left==\null) &&
-  @       (k.up==stop ==> k.p_parent==\null)&&
-  @       (k.right==stop ==> k.p_right==\null);
+  @       (k.p_parent != \null ==> valid_TreeWalk_ptr{L}(k.p_parent)) &&
+  @       (k.p_left != \null ==> valid_TreeWalk_ptr{L}(k.p_left)) &&
+  @       (k.p_right != \null ==> valid_TreeWalk_ptr{L}(k.p_right));
   @
   @ axiom valid_TreeWalk_ptr_def{L}: \forall TreeWalk *k; 
   @         valid_TreeWalk_ptr{L}(k) <==> \valid{L}(k) && valid_TreeWalk{L}(*k);
@@ -67,4 +61,68 @@ void struct_change_nothing2(Memory*);
   @ lemma valid_TreeWalk_ptr_to_valid{L}: \forall TreeWalk *k;
   @        valid_TreeWalk_ptr{L}(k) ==> \valid{L}(k);
   @
+  @*/
+
+/*@ axiomatic Ax_TreeWalk_Exists_Value { 
+  @    predicate TreeWalk_Exists_Value{L}(TreeWalk *k, int val)=
+  @               (k!=\null && valid_TreeWalk_ptr{L}(k) && 
+  @               (k->key == val || TreeWalk_Exists_Value{L}(k->p_left,val) ||
+  @                                 TreeWalk_Exists_Value{L}(k->p_right,val))); 
+  @   
+  @    predicate TreeWalk_Exists_Value_Count{L}(TreeWalk *k, int val, int n)= (k==\null && n==0) ||
+  @             (
+  @		k!=\null && valid_TreeWalk_ptr{L}(k) && 
+  @             	(
+  @			
+  @			( k->key != val && \exists int i, int j; 0<=i<=n && 0<=j<=n && i+j==n &&
+  @			TreeWalk_Exists_Value_Count{L}(k->p_left,val,i) && TreeWalk_Exists_Value_Count{L}(k->p_right,val,j) )
+  @			||
+  @			( k->key == val && \exists int i, int j; 0<=i<=(n-1) && 0<=j<=(n-1) && i+j==(n-1) && 
+  @			TreeWalk_Exists_Value_Count{L}(k->p_left,val,i) && TreeWalk_Exists_Value_Count{L}(k->p_right,val,j) )
+  @
+  @			)
+  @
+  @		);
+  @
+  @ } //Ax_TreeWalk_Exists_Value
+  @
+  @ lemma TreeWalk_NoValue_In_Null_Tree{L}: 
+  @             \forall int v; ! TreeWalk_Exists_Value{L}(\null,v);
+  @
+  @ lemma TreeWalk_Exists_Value_Recursive{L}:
+  @              \forall TreeWalk *k, int v;
+  @               (valid_TreeWalk_ptr{L}(k) ==> 
+  @        (TreeWalk_Exists_Value{L}(k,v) <==> ((k!=\null) && 
+  @						((k->key==v)||
+  @                                             TreeWalk_Exists_Value{L}(k->p_left,v)||    
+  @                                             TreeWalk_Exists_Value{L}(k->p_right,v)))));
+  @
+  @ lemma TreeWalk_Zero_Values_In_Null_Tree{L}:
+  @ 	\forall int v, int z; z==0 ==> TreeWalk_Exists_Value_Count{L}(\null,v,z);
+  @	
+  @ lemma TreeWalk_Exists_Value_Count_Recursive{L}:
+  @              \forall TreeWalk *k, int v, int n;
+  @              (   valid_TreeWalk_ptr{L}(k) ==> 
+  @        (  TreeWalk_Exists_Value_Count{L}(k,v,n) <==> ((k==\null && n==0) || ((k!=\null) && 
+  @			(
+  @			
+  @			( k->key != v && \exists int i, int j; 0<=i<=n && 0<=j<=n && i+j==n &&
+  @			TreeWalk_Exists_Value_Count{L}(k->p_left,v,i) && TreeWalk_Exists_Value_Count{L}(k->p_right,v,j) )
+  @			||
+  @			( k->key == v && \exists int i, int j; 0<=i<=(n-1) && 0<=j<=(n-1) && i+j==(n-1) && 
+  @			TreeWalk_Exists_Value_Count{L}(k->p_left,v,i) && TreeWalk_Exists_Value_Count{L}(k->p_right,v,j) )
+  @
+  @			)))  )   );
+  @
+  @
+  @
+  @
+  @
+  @
+  @
+  @
+  @
+  @
+  @
+  @								
   @*/
