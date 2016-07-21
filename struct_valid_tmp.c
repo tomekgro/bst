ba098a93b00ff5b6a181aa09fbef5955b25d3a2f
tomekgro <grodek.tomasz@gmail.com>
2016-07-21 16:09:29 +0200

    Update struct_valid_tmp.c

--- struct_valid_tmp.c
+++ struct_valid_tmp.c
@@ -9,10 +9,14 @@
 void struct_change_nothing1(Memory*);
 void struct_change_nothing2(Memory*);
 
-void multi_insert(TreeWalk *, TreeWalk *);
+void multi_insert(TreeWalk *, TreeWalk *); // inserts a number to the bst tree, possibly a number already existing in the tree
 TreeWalk *get_TreeWalk();
-int CountVal(TreeWalk *, int, int);
+int CountVal(TreeWalk *, int, int); // returns the numbers of appearences of a requested key
 void updatedepth (TreeWalk *);
+void updateweight (TreeWalk *);
+void deletekey (TreeWalk **, int);
+void deletenode (TreeWalk **);
+void deleteTreeWalk (TreeWalk **);
 
 
 TreeWalk *root;
@@ -50,13 +54,17 @@ void main()
 			printf(" time(s) in the requested tree.");
 			break;
 	
+		case 3: 
+			scanf("%d", &deletedkey);
+			deletekey(&root, deletedkey);
+			break;
    	}
 
    } while (choice != 0);
 }
 // FUNCTIONS FROM BSTTG
 
-/* getiing a new node  */
+/* getting a new node  */
 TreeWalk *get_TreeWalk()
 {
 	TreeWalk *temp;
@@ -65,11 +73,12 @@ TreeWalk *get_TreeWalk()
 	temp->p_right = NULL;
 	temp->p_parent = NULL;
 	temp->depth = 0;
-	temp->weight = 0;
+	temp->weight = 1;
 	return temp;
 }
 
 /* inserting a node to an existing tree, multiple version */
+// TO PROVE: increases CountVal by 1.
 
 /*@ requires \valid(root) && valid_TreeWalk_ptr(root) && \valid(new_TreeWalk) && valid_TreeWalk_ptr(new_TreeWalk);
   @ assigns \nothing;
@@ -100,7 +109,7 @@ void multi_insert(TreeWalk *root, TreeWalk *new_TreeWalk)
 	}
 
 }
-
+// REDUNDANT. I leave it just in case.
 /* @ requires \valid(root) && valid_TreeWalk_ptr(root);
    @ assigns \nothing;
    @ */
@@ -121,6 +130,183 @@ void updatedepth(TreeWalk *root) // It starts with the given node and goes upwar
 	}
 }
 
+// Updates all changed weights of nodes after inserting and/or removing nodes.
+// TO PROVE: ???
+
+void updateweight(TreeWalk *root) // It starts with the given node and goes upwards, to the root - updating each depth.
+{
+	TreeWalk *temp;
+	temp = root;
+	while (temp != NULL)
+	{
+		if (temp->p_left == NULL && temp->p_right == NULL) temp->weight = 0;
+		else if (temp->p_left == NULL) temp->depth = (temp->p_right->weight +1);
+		else if (temp->p_right == NULL) temp->depth = (temp->p_left->weight +1);
+		else temp->weight = (temp->p_right->weight + temp->p_left->weight +1);
+		temp = temp->p_parent;
+
+	}
+}
+
+/* deleting a node with the given key */
+// TO PROVE: decreases CountVal by 1.
+
+void deletekey (TreeWalk **rootpointer, int deletedkey) // Finds the first node with a given key in the tree with a root *rootpointer and runs the function "deletenode".
+{
+	TreeWalk *temp;
+	TreeWalk *parent;
+	parent = NULL;
+	temp = *rootpointer;
+	int deleted;
+	deleted = 0;
+	if (*rootpointer == NULL);
+	else if ((*rootpointer)->key == deletedkey) 
+	{
+		deletenode(rootpointer);
+		deleted = 1;
+	}		
+
+	else if ((*rootpointer)->key > deletedkey && deletedkey == (*rootpointer)->p_left->key)
+	{
+		deletenode(&(*rootpointer)->p_left);
+		deleted = 1;
+	}
+
+	else if ((*rootpointer)->key < deletedkey && deletedkey == (*rootpointer)->p_right->key)
+	{
+		deletenode(&(*rootpointer)->p_right);
+		deleted = 1;
+	}
+
+	else
+	{
+		while (deleted == 0 && temp != NULL) 
+		{
+			if (temp->key > deletedkey) 
+			{
+				parent = temp;
+				temp = temp->p_left;
+			}
+			else if (temp->key < deletedkey)
+			{
+				parent = temp;
+				temp = temp->p_right;
+			}
+			else /* temp->key == deletedkey */
+			{
+				if (deletedkey == parent->p_left->key) deletenode(&parent->p_left);
+				else deletenode(&parent->p_right);
+				deleted = 1;
+			}
+		}
+	}
+
+//	if (deleted == 0) printf("\nThe given key doesn't appear in the tree. It was NOT deleted.");
+}
+
+
+// Deletes a given node. "deletedroot" is an address of a pointer to a deleted root.
+// TO PROVE: ??? Something about weights?
+
+void deletenode (TreeWalk **deletedroot) 
+{
+	TreeWalk *temp;
+	TreeWalk *parent;
+	if ((*deletedroot)->p_left == NULL && (*deletedroot)->p_right == NULL)
+	{
+		free(*deletedroot);
+		*deletedroot = NULL;
+	}
+	else if ((*deletedroot)->p_left == NULL)
+	{
+		temp = (*deletedroot)->p_right;
+		free(*deletedroot);
+		temp->p_parent = NULL;
+		*deletedroot = temp;
+		//No change of depths is needed.
+	}
+	else if ((*deletedroot)->p_right == NULL)
+	{
+		temp = (*deletedroot)->p_left;
+		free(*deletedroot);
+		temp->p_parent = NULL;
+		*deletedroot = temp;
+		//No change of depths is needed.
+	}
+	else // Both sons of *deletedroot are not NULL
+	{	
+
+		if ((*deletedroot)->p_left->weight > (*deletedroot)->p_right->weight) // The left son of *deletedroot will be a new root.
+		{
+			if ((*deletedroot)->p_left->p_right == NULL) 
+			{
+				(*deletedroot)->p_left->p_right = (*deletedroot)->p_right;
+				(*deletedroot)->p_right->p_parent = (*deletedroot)->p_left;
+				temp = (*deletedroot)->p_left;
+				free(*deletedroot);
+				temp->p_parent = NULL;
+				updateweight(temp); //Updates the weight of temp, which is a new root 
+				*deletedroot = temp;
+			}
+			else
+			{
+				parent = (*deletedroot)->p_left;
+				temp = parent->p_right;
+				while (temp->p_right != NULL)
+				{
+					parent = temp;
+					temp = temp->p_right;
+				} /* temp now points a node with the biggest key in a left subtree of *deletedroot - a new root */
+				temp->p_right = (*deletedroot)->p_right;
+				(*deletedroot)->p_right->p_parent = temp;
+				parent->p_right = temp->p_left;
+				if (temp->p_left != NULL) temp->p_left->p_parent = parent;
+				temp->p_left = (*deletedroot)->p_left;
+				(*deletedroot)->p_left->p_parent = temp;
+				free(*deletedroot);
+				temp->p_parent = NULL;
+				updateweight(parent);
+				updateweight(temp);
+				*deletedroot = temp; 
+			}
+		}
+
+		else // root->p_right is a default new root. Analogously as above.
+        	{
+            		if ((*deletedroot)->p_right->p_left == NULL)
+			{	
+                		(*deletedroot)->p_right->p_left = (*deletedroot)->p_left;
+				(*deletedroot)->p_left->p_parent = (*deletedroot)->p_right;
+				temp = (*deletedroot)->p_right;
+				free(*deletedroot);
+				temp->p_parent = NULL;
+                		*deletedroot = temp;
+				updateweight(temp);
+			}
+            		else
+            		{
+                		parent = (*deletedroot)->p_right;
+		                temp = parent->p_left;
+                		while (temp->p_left != NULL)
+                		{
+                    			parent = temp;
+                    			temp = temp->p_left;
+		                } /* temp now points the new root */
+        	        	temp->p_left = (*deletedroot)->p_left;
+				(*deletedroot)->p_left->p_parent = temp;
+               		 	parent->p_left = temp->p_right;
+				if (temp->p_right != NULL) temp->p_right->p_parent = parent;
+				temp->p_right = (*deletedroot)->p_right;
+				(*deletedroot)->p_right->p_parent = temp;
+				free(*deletedroot);
+				temp->p_parent = NULL;
+				updateweight(parent);
+				updateweight(temp);
+	       		        *deletedroot = temp;
+            		}
+        	}
+	}
+}
 
 
 // FUNCTIONS FROM STRUCT_VALID_TMP
